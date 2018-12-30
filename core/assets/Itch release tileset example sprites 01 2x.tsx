dddd620b86b009b439c7b61509e8bd6b1f666ebe
SeiRiuS <andsei91@gmail.com>
2018-12-30 14:34:05 +0100

    Box2D Scaling

---
+++ core/assets/Itch release tileset example sprites 01 2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Itch release tileset example sprites 01 2x" tilewidth="32" tileheight="32" tilecount="408" columns="17">
+ <image source="Itch release tileset example sprites 01 2x.png" width="544" height="784"/>
+</tileset>
